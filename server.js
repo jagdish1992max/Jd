7617f6d50a0df2331e3280b460c414e1d674dc34
Jd Butiya <jagdishjd6@gmail.com>
2026-04-21 00:44:55 +0530

    server.js

--- server.js
+++ server.js
@@ -3,15 +3,9 @@ const path = require("path");
 
 const app = express();
 
-// serve static files
-app.use(express.static(__dirname));
-
-// MUST: homepage should serve HTML file
 app.get("/", (req, res) => {
   res.sendFile(path.join(__dirname, "index.html"));
 });
 
 const PORT = process.env.PORT || 3000;
-app.listen(PORT, () => {
-  console.log("Server running");
-});
+app.listen(PORT);
